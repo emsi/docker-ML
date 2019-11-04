30939a0d7f36161d9dcd923b8e7cd954b9df75bd
Mariusz Woloszyn <emsi@users.noreply.github.com>
2019-11-04 19:49:27 +0100

    Add tf 2.0

--- run_as_user.sh
+++ run_as_user.sh
@@ -1,3 +1,7 @@
+#!/bin/bash 
+
+export TF_VERSION="1.14.0-gpu-py3"
+
 docker run --runtime=nvidia --rm \
         -p8888:8888 \
 	-e HOME=${HOME} \
@@ -5,5 +9,6 @@ docker run --runtime=nvidia --rm \
 	-u $(id -u):$(id -g) \
 	-v /etc/passwd:/etc/passwd:ro \
         -v ${HOME}:${HOME} \
-        --name dl-1.14 \
-        emsi/deep-learning:1.14.0-gpu-py3 jupyter-lab --ip=0.0.0.0 
+        --name dl-${TF_VERSION} \
+	-e "VIRTUAL_HOST=dl.do.qpqp01.pl" -e "LETSENCRYPT_HOST=dl.do.qpqp01.pl" -e "LETSENCRYPT_EMAIL=emsi@qpqp01.pl" -e "VIRTUAL_PORT=8888" \
+        emsi/deep-learning:${TF_VERSION} jupyter-lab --ip=0.0.0.0 
