ed1e1c9590d1858c19cc55ebc05cefc6cb0bcfaf
Mariusz Woloszyn <emsi@users.noreply.github.com>
2022-11-21 16:33:58 +0000

    Enable sudo from within the container

--- run_as_user.sh
+++ run_as_user.sh
@@ -12,6 +12,14 @@ COMPOSE_USER_ID=$(id -u)
 COMPOSE_GROUP_ID=$(id -g)
 EOF
 
+cat > "${script_dir}/sudoers.sh" << EOF
+#!/bin/bash
+
+echo "$(whoami) ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/sudoers
+chown 0:0 /etc/sudoers.d/sudoers
+EOF
+chmod +x sudoers.sh
+
 
 if [ `which dokcer-compose` ]; then
 	compose='docker-compose'
@@ -22,15 +30,3 @@ fi
 $compose up -d
 
 $compose logs -f
-
-
-#docker run --runtime=nvidia --rm \
-#        -p8888:8888 \
-#	-e HOME=${HOME} \
-#        -w ${HOME} \
-#	-u $(id -u):$(id -g) \
-#	-v /etc/passwd:/etc/passwd:ro \
-#        -v ${HOME}:${HOME} \
-#        --name dl-${TF_VERSION} \
-#	-e "VIRTUAL_HOST=dl.do.qpqp01.pl" -e "LETSENCRYPT_HOST=dl.do.qpqp01.pl" -e "LETSENCRYPT_EMAIL=emsi@qpqp01.pl" -e "VIRTUAL_PORT=8888" \
-#        emsi/deep-learning:${TF_VERSION} jupyter-lab --ip=0.0.0.0 
