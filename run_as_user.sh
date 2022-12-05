b89bc7043c4debcde3be7820efcaae39b074abb3
Mariusz Woloszyn <emsi@users.noreply.github.com>
2022-12-05 13:24:10 +0000

    Accept py & tf versions sets in .env; add comments

--- run_as_user.sh
+++ run_as_user.sh
@@ -4,17 +4,27 @@ set -Eeuo pipefail
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
+# source existing env values
+. "${script_dir}"/.env
+
+# set default values
+PY_VERSION="${PY_VERSION:-"3.10"}"
+TF_VERSION="${TF_VERSION:-"2.10"}"
+
+# refresh repo
 echo "$(cd "${script_dir}"; git pull)" >> /dev/null
 
+# set variables for  uid/gid and prefix project name with username
 cat > "${script_dir}/.env" << EOF
-PY_VERSION="3.10"
-TF_VERSION="2.10"
+PY_VERSION="${PY_VERSION}"
+TF_VERSION="${TF_VERSION}"
 
 COMPOSE_USER_ID=$(id -u)
 COMPOSE_GROUP_ID=$(id -g)
 COMPOSE_PROJECT_NAME="$(whoami)-ml"
 EOF
 
+# create appropriate sudoers.sh script
 cat > "${script_dir}/sudoers.sh" << EOF
 #!/bin/bash
 
