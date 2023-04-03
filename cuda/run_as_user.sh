6d0e155bc74fdf821a54aa096a8e6c9eb3fecabd
Mariusz Woloszyn <emsi@users.noreply.github.com>
2023-04-03 18:44:20 +0200

    container based on cuda images

---
+++ cuda/run_as_user.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+# set default values
+CUDA_VERSION="11.7.1-cudnn8-runtime-ubuntu22.04"
+TF_VERSION="${TF_VERSION:-"2.10"}"
+
+# source existing env values
+. "${script_dir}"/.env 2>/dev/null || true
+
+# refresh repo
+echo "$(cd "${script_dir}"; git pull)" >> /dev/null
+
+# set variables for  uid/gid and prefix project name with username
+cat > "${script_dir}/.env" << EOF
+CUDA_VERSION="${CUDA_VERSION}"
+TF_VERSION="${TF_VERSION}"
+
+COMPOSE_USER_ID=$(id -u)
+COMPOSE_GROUP_ID=$(id -g)
+COMPOSE_PROJECT_NAME="$(whoami)-ml-cuda"
+EOF
+
+# create appropriate sudoers.sh script
+cat > "${script_dir}/sudoers.sh" << EOF
+#!/bin/bash
+
+echo "$(whoami) ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/sudoers
+chown 0:0 /etc/sudoers.d/sudoers
+
+mkdir -p /home/$(whoami)/
+chown -R $(id -u):$(id -g) /home/$(whoami)/
+EOF
+chmod +x sudoers.sh
+
+
+if [ "$(which docker-compose)" ]; then
+	compose='docker-compose'
+else
+	compose='docker compose'
+fi
+
+$compose up -d
+
+$compose logs -f
